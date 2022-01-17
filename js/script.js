78474562e731443d275c2ca9ab91eb03fa71dc4e
SebastianCieslik <ssssss-94@o2.pl>
2022-01-17 12:56:51 +0100

    script.js working remove and sort list

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 
 //let countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-//let checked = document.querySelector(".submited");
 let countries = ["Afghanistan","Albania","Algeria","Andorra","Angola"];
 const checked = document.querySelector(".submited");
 const activeList = [];
-//const countriesWithoutActive = countries.filter(country => !activeList.find(country))
-//const angolaIndex = countries.indexOf("Angola");
-//const fuckAngola = countries.filter((country, index) => index !== angolaIndex );
 const devArea = document.querySelector(".dev-area");
 
-// moveBackward = ( =>
-//  <a id="myLink" href="#" onclick="MyFunction();">link text</a>)
+
+const moveBackward = (name) => {
+
+  console.log(name);
+  const countyident = document.getElementById(name);
+  console.log(countyident);
+  countyident.parentNode.removeChild(countyident);
+  //
+  countries.push(name);
+  countries.sort();
+  let countryIndex = activeList.indexOf(name);
+  activeList.splice(countryIndex,1);
+
+};
+
+
 
 const createCountry = (name) => {
     const item = document.createElement("li");
@@ -20,9 +30,16 @@ const createCountry = (name) => {
     item2.textContent = "X";
     item.appendChild(item3);
     item.appendChild(item2);
+    item.setAttribute("value",name);
+    item2.setAttribute("value",name);
+    item.setAttribute("id",name);
+    item3.setAttribute("value",name);
+    item2.addEventListener("click", function() {
+      moveBackward(name);
+    });
   	return item;
 };
-  
+
 const refreshCountries = () => {
   checked.textContent = '';
   activeList.map(country => checked.appendChild(createCountry(country)));
@@ -63,15 +80,10 @@ const refreshCountries = () => {
                   activeList.push(countryName);
                 //splice
                 const countryIndex = countries.indexOf(countryName);
-                countries.splice(countryIndex,countryIndex+1);
-                  console.log(activeList);
-                  console.log(countryIndex);
+                countries.splice(countryIndex,1);
+                  console.log(activeList);;
                   refreshCountries();
-                  //MoveElement();
 
-
-
-                /*insert the value for the autocomplete text field:*/
                 inp.value = this.getElementsByTagName("input")[0].value;
                 /*close the list of autocompleted values,
                 (or any other open lists of autocompleted values:*/
