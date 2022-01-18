763540dd7380fbdeb6ef72b8538137d7ab6ec909
SebastianCieslik <ssssss-94@o2.pl>
2022-01-18 18:19:35 +0100

    Finished

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-
-let countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-//let countries = ["Afghanistan","Albania","Algeria","Andorra","Angola"];
-const checked = document.querySelector(".submited");
+//const initialCountries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+const countries = ["Afghanistan","Albania","Algeria","Andorra","Angola"];
+const activeListElement = document.querySelector(".submited");
 const activeList = [];
-var typeName = "";
 const input = document.querySelector(".myInput");
 
 
@@ -11,7 +9,7 @@ const input = document.querySelector(".myInput");
 const moveBackward = (name) => {
   const countyident = document.getElementById(name);
   countyident.parentNode.removeChild(countyident);
-  if(countries.includes(name) == false){
+  if(!countries.includes(name)){
   countries.push(name);
   countries.sort();
   let countryIndex = activeList.indexOf(name);
@@ -22,97 +20,75 @@ const moveBackward = (name) => {
 
 
 const createCountry = (name) => {
-    const item = document.createElement("li");
-    const item2 = document.createElement("button");
-    const item3 = document.createElement("span");
-    item3.textContent = name;
-    item2.textContent = "✕";
-    item2.setAttribute("class","deleteButton");
-    item.appendChild(item3);
-    item.appendChild(item2);
-    item.setAttribute("value",name);
-    item2.setAttribute("value",name);
-    item.setAttribute("id",name);
-    item3.setAttribute("value",name);
-    item3.setAttribute("id","li");
-    item2.addEventListener("click", function() {
-      moveBackward(name);
+    const listElement = document.createElement("li");
+    const removeButton = document.createElement("button");
+    const textElement = document.createElement("span");
+  
+    textElement.textContent = name;
+    textElement.setAttribute("value",name);
+  
+    removeButton.textContent = "✕";
+    removeButton.classList.add("deleteButton");
+    removeButton.setAttribute("value",name);
+    removeButton.addEventListener("click", function() {
+      if(initialCountries.includes(name)) {
+        moveBackward(name);
+      } else {
+          let countryIndex = activeList.indexOf(name);
+          activeList.splice(countryIndex,1);
+        	refreshCountries();
+      }
     });
-
-  	return item;
+  
+    listElement.appendChild(textElement);
+    listElement.appendChild(removeButton);
+    listElement.setAttribute("value",name);
+    listElement.setAttribute("id",name);
+ 
+  	return listElement;
 };
+  
 const addNewCountry = () => {
-
   if(input.value){
     const name = input.value;
-    const item = document.createElement("li");
-    const item2 = document.createElement("button");
-    const item3 = document.createElement("span");
-    item3.textContent = name;
-    item2.textContent = "✕";
-    item2.setAttribute("class","deleteButton");
-    item.appendChild(item3);
-    item.appendChild(item2);
-    item.setAttribute("value",name);
-    item2.setAttribute("value",name);
-    item.setAttribute("id",name);
-    item3.setAttribute("value",name);
-    item3.setAttribute("id","li");
-    item2.addEventListener("click", function() {
-      moveBackward(name);
-    });
+    const isAlreadyAdded = activeList.includes(name);
+    if (isAlreadyAdded) return;
     activeList.push(name);
-    checked.appendChild(item);
-
+    refreshCountries();
+    input.value = "";
   };
 }
 
 const refreshCountries = () => {
-  checked.textContent = '';
-  activeList.map(country => checked.appendChild(createCountry(country)));
+ activeListElement.textContent = '';
+  activeList.map(country => activeListElement.appendChild(createCountry(country)));
 }
 
 
  function autocomplete(inp, arr) {
-    /*the autocomplete function takes two arguments,
-    the text field element and an array of possible autocompleted values:*/
+
     var currentFocus;
-    /*execute a function when someone writes in the text field:*/
     inp.addEventListener("input", function(e) {
-        var a, b, i, val = this.value;
-        /*close any already open lists of autocompleted values*/
+        var listWrapper, listItem, val = this.value;
         closeAllLists();
-
-        typeName = this.value;
-
+      
         if (!val) { return false;}
         currentFocus = -1;
-        /*create a DIV element that will contain the items (values):*/
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        /*append the DIV element as a child of the autocomplete container:*/
-        this.parentNode.appendChild(a);
-        /*for each item in the array...*/
-        for (i = 0; i < arr.length; i++) {
+
+        listWrapper = document.createElement("DIV");
+        listWrapper.setAttribute("id", `${this.id}autocomplete-list`);
+        listWrapper.classList.add("autocomplete-items");
+      
+        this.parentNode.appendChild(listWrapper);
+      
+        for (let i = 0; i < arr.length; i++) {
           /*check if the item starts with the same letters as the text field value:*/
-          
-
           if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-            /*create a DIV element for each matching element:*/
-            b = document.createElement("DIV");
-//
-            
-            
-
-//
-            /*make the matching letters bold:*/
-            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-            b.innerHTML += arr[i].substr(val.length);
-            /*insert a input field that will hold the current array item's value:*/
-            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-            /*execute a function when someone clicks on the item value (DIV element):*/
-                b.addEventListener("click", function(e) {
+            listItem = document.createElement("DIV");
+            listItem.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            listItem.innerHTML += arr[i].substr(val.length);
+            listItem.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                listItem.addEventListener("click", function(e) {
                   const countryName = this.getElementsByTagName("input")[0].value;
                   activeList.push(countryName);
                 const countryIndex = countries.indexOf(countryName);
@@ -122,63 +98,53 @@ const refreshCountries = () => {
                 closeAllLists();
                 input.value = "";
             });
-            a.appendChild(b);
+            listWrapper.appendChild(listItem);
           }6
         }
     });
-    /*execute a function presses a key on the keyboard:*/
     inp.addEventListener("keydown", function(e) {
-        var x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        if (e.keyCode == 40) {
-          /*If the arrow DOWN key is pressed,
-          increase the currentFocus variable:*/
+        let autocompleteList = document.getElementById(this.id + "autocomplete-list");
+        if (autocompleteList){
+          autocompleteList = autocompleteList.getElementsByTagName("div");
+        }
+        if (e.keyCode == 40) { // DOWN KEY
           currentFocus++;
-          /*and and make the current item more visible:*/
-          addActive(x);
-        } else if (e.keyCode == 38) { //up
-          /*If the arrow UP key is pressed,
-          decrease the currentFocus variable:*/
+          addActive(autocompleteList);
+        } else if (e.keyCode == 38) { // UP KEY
           currentFocus--;
-          /*and and make the current item more visible:*/
-          addActive(x);
-        } else if (e.keyCode == 13) {
-          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          addActive(autocompleteList);
+        } else if (e.keyCode == 13) {//ENTER KEY
           e.preventDefault();
           if (currentFocus > -1) {
-            /*and simulate a click on the "active" item:*/
-            if (x) x[currentFocus].click();
+            if (autocompleteList) autocompleteList[currentFocus].click();
           }
         }
     });
-    function addActive(x) {
+    function addActive(item) {
       /*a function to classify an item as "active":*/
-      if (!x) return false;
-      /*start by removing the "active" class on all items:*/
-      removeActive(x);
-      if (currentFocus >= x.length) currentFocus = 0;
-      if (currentFocus < 0) currentFocus = (x.length - 1);
-      /*add class "autocomplete-active":*/
-      x[currentFocus].classList.add("autocomplete-active");
+      if (!item) return false;
+      removeActive(item);
+      if (currentFocus >= item.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (item.length - 1);
+      item[currentFocus].classList.add("autocomplete-active");
     }
-    function removeActive(x) {
+    function removeActive(item) {
       /*a function to remove the "active" class from all autocomplete items:*/
-      for (var i = 0; i < x.length; i++) {
-        x[i].classList.remove("autocomplete-active");
+      for (let i = 0; i < item.length; i++) {
+        item[i].classList.remove("autocomplete-active");
       }
     }
     function closeAllLists(elmnt) {
-      /*close all autocomplete lists in the document,
-      except the one passed as an argument:*/
-      var x = document.getElementsByClassName("autocomplete-items");
-      for (var i = 0; i < x.length; i++) {
-        if (elmnt != x[i] && elmnt != inp) {
-        x[i].parentNode.removeChild(x[i]);
+      let list = document.getElementsByClassName("autocomplete-items");
+      for (let i = 0; i < list.length; i++) {
+        if (elmnt != list[i] && elmnt != inp) {
+        list[i].parentNode.removeChild(list[i]);
       }
     }
   }
-  /*execute a function when someone clicks in the document:*/
   document.addEventListener("click", function (e) {
       closeAllLists(e.target);
   });
   } 
+  
+ autocomplete(document.getElementById("myInput"), countries);
